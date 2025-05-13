011f5bce35657a8b3ea866f7faa293812e8a9e34
gregory <jeanlouisgregory21@gmail.com>
2025-05-12 20:34:40 -0400

    added the  handles errors reading files with a 500 error message

--- index.js
+++ index.js
@@ -1,41 +1,46 @@
-const  http = require('http');
+const http = require('http');
 const fs = require('fs');
 const path = require('path');
-
 
 const PORT = 8080;
 
 console.log(__dirname);
 
-
 const server = http.createServer((req, res) => {
-    if(req.url === '/') {
-        fs.readFile(path.join(__dirname, 'pages/home.html'), (err,data)=>{
-            res.writeHead(200,{'Content-Type': 'text/html'});
-            res.end(data);
-        })
+    if (req.url === '/') {
+        fs.readFile(path.join(__dirname, 'pages/home.html'), (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/html' });
+                res.end('<h1>500 Internal Server Error</h1><p>Unable to load the page.</p>');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            }
+        });
+    } else if (req.url === '/about') {
+        fs.readFile(path.join(__dirname, 'pages/about.html'), (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/html' });
+                res.end('<h1>500 Internal Server Error</h1><p>Unable to load the page.</p>');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            }
+        });
+    } else if (req.url === '/api') {
+        fs.readFile(path.join(__dirname, 'data/users.json'), (err, data_) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ error: 'Unable to load data' }));
+            } else {
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(data_);
+            }
+        });
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end('<h1>404 Not Found</h1>');
     }
-    else if(req.url === '/about') {
-        
-        fs.readFile(path.join(__dirname, 'pages/about.html'), (err,data) =>{
-            res.writeHead(200,{'Content-Type': 'text/html'});
-            res.end(data);
-        })
-    }
-    else if ( req.url === '/api'){
-        fs.readFile (path.join(__dirname, 'data/users.json'), (err,data_)=>{
-            res.writeHead(200,{'Content-Type': 'application/json'});
-            res.end(data_);
-        })
-    }
-   
-    else {
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        res.end('<h1>404 Not Found</h1>');  
-        res.end();
-    }
-
-
 });
 
 server.listen(PORT, () => {
